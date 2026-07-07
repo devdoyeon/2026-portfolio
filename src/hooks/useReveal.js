2b82a1455f1b1f88294ce19e507af92f6353fa84
devdoyeon <doyeonyou@naver.com>
2026-07-07 18:54:39 +0900

    edit: 스크롤 리빌 절제 (등장 거리·시간 축소, 트리거 소폭 앞당김)

--- src/hooks/useReveal.js
+++ src/hooks/useReveal.js
@@ -33,7 +33,7 @@ export function useRevealAll(deps = []) {
           }
         });
       },
-      { threshold: 0.12, rootMargin: '0px 0px -6% 0px' }
+      { threshold: 0.08, rootMargin: '0px 0px 4% 0px' }
     );
     els.forEach((el) => io.observe(el));
     return () => io.disconnect();
